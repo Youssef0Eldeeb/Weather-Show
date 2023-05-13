304596a0be0aefdd70c43a644ac8b30f353d62e1
Youssef0Eldeeb <youssefeldeeb3109@gmail.com>
2023-05-13 11:51:19 +0200

    Initial Commit

---
+++ Weather Show/Weather_ShowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_ShowApp.swift
+//  Weather Show
+//
+//  Created by Youssef Eldeeb on 13/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_ShowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
